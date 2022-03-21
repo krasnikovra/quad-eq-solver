7198802d098f9d7139868e67d30e04ed9ded4461
krasnikovra <roma_krasnikov02@mail.ru>
2022-03-21 17:46:40 +0300

    not-a-quad eqs now solvable

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <complex>
 
-using namespace std;
+#define NO_ROOTS QuadEqRoots(0, 0, 0)
+#define INF_ROOTS QuadEqRoots(0, 0, INT_MAX)
 
-using QuadEqRoots = pair<complex<double>, complex<double>>;
+using namespace std;
+using Complex = complex<double>;
 
 struct QuadEq {
     double a, b, c;
+    QuadEq() : a(0), b(0), c(0) {}
+    QuadEq(const double a_, const double b_, const double c_) : a(a_), b(b_), c(c_) {}
 };
 
-QuadEqRoots SolveQuadraticEquation(const QuadEq& eq, bool* isOk) {
+struct QuadEqRoots {
+    Complex x1, x2;
+    size_t cnt;
+    QuadEqRoots(const Complex& x1_, const Complex& x2_, const size_t cnt_) :
+        x1(x1_), x2(x2_), cnt(cnt_) {}
+};
+
+QuadEqRoots SolveQuadraticEquation(const QuadEq& eq) {
     if (eq.a == 0.0) {
-        *isOk = false;
-        return QuadEqRoots(0, 0);
+        if (eq.b == 0.0)
+            return eq.c == 0.0 ? INF_ROOTS : NO_ROOTS;
+        Complex root(-eq.c / eq.b);
+        return QuadEqRoots(root, root, 1);
     }
-    complex<double> discriminantSqrt = sqrt(complex<double>(eq.b * eq.b - 4 * eq.a * eq.c));
-    *isOk = true;
-    return QuadEqRoots((-eq.b + discriminantSqrt) / (2 * eq.a), (-eq.b - discriminantSqrt) / (2 * eq.a));
+    Complex discriminantSqrt = sqrt(Complex(eq.b * eq.b - 4 * eq.a * eq.c));
+    // here if we get two equal roots its degree is 2
+    // which states x^2=0 and x=0 will have different QuadEqRoots
+    return QuadEqRoots((-eq.b + discriminantSqrt) / (2 * eq.a),
+        (-eq.b - discriminantSqrt) / (2 * eq.a), 2);
+}
+
+bool operator==(const Complex& a, const Complex& b) {
+    return a.real() == b.real() && a.imag() == b.imag();
+}
+
+bool operator==(const QuadEqRoots& a, const QuadEqRoots& b) {
+    // roots can be exchanged but are being assumed still equal
+    return ((a.x1 == b.x1 && a.x2 == b.x2) || (a.x1 == b.x2 && a.x2 == b.x1)) && a.cnt == b.cnt;
 }
 
 ostream& operator<<(ostream& stream, const complex<double>& complex) {
@@ -34,25 +58,42 @@ QuadEq ReadEqFromCin() {
 }
 
 void PrintQuadEqRootsFancy(const QuadEqRoots& roots) {
-    if (roots.first == roots.second)
-        cout << "The root is " << roots.first << endl;
-    else
-        cout << "The roots are " << roots.first << " and " << roots.second << endl;
+    // a=b=0 exceptional cases
+    if (roots == INF_ROOTS) {
+        cout << "Every complex is a root" << endl;
+        return;
+    }
+    else if (roots == NO_ROOTS) {
+        cout << "Equation has no roots" << endl;
+        return;
+    }
+    // linear case
+    else if (roots.cnt == 1) {
+        cout << "The root is " << roots.x1 << endl;
+        return;
+    }
+    // common quad eq case
+    else {
+        if (roots.x1 == roots.x2) {
+            cout << "The 2nd degree root is " << roots.x1 << endl;
+            return;
+        }
+        else {
+            cout << "The roots are " << roots.x1 << " and " << roots.x2 << endl;
+            return;
+        }
+    }
 }
 
-bool PrintQuadEqSolution(const QuadEq& eq) {
-    bool isOk = false;
-    QuadEqRoots roots = SolveQuadraticEquation(eq, &isOk);
-    if (isOk)
-        PrintQuadEqRootsFancy(roots);
-    else
-        cout << "Not a quadratic equation" << endl;
-    return isOk;
+void PrintQuadEqSolution(const QuadEq& eq) {
+    QuadEqRoots roots = SolveQuadraticEquation(eq);
+    PrintQuadEqRootsFancy(roots);
 }
 
 int main() {
     // we are getting some quad eq from user
     QuadEq eq = ReadEqFromCin();
     // and giving user back its roots
-    return PrintQuadEqSolution(eq) ? 0 : -1;
+    PrintQuadEqSolution(eq);
+    return 0;
 }
