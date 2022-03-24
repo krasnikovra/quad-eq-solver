fd5910182818468706e826cb299f3570861f1582
krasnikovra <roma_krasnikov02@mail.ru>
2022-03-24 16:11:47 +0300

    types declarations moved to Utils.h

---
+++ include/Utils.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <complex>
+
+using Complex = std::complex<double>;
+
+class QuadEqRoots {
+public:
+    enum class Count {
+        NO,
+        ONE,
+        TWO,
+        INF
+    };
+
+    QuadEqRoots(const Complex& x) : _x1(x), _x2(x), _count(Count::ONE) {}
+    QuadEqRoots(const Complex& x1, const Complex& x2) : _x1(x1), _x2(x2),
+        _count(x1 == x2 ? Count::ONE : Count::TWO) {}
+    Complex GetX1() const { return _x1; }
+    Complex GetX2() const { return _x2; }
+    Count GetCount() const { return _count; }
+    static QuadEqRoots NoRoots() { return QuadEqRoots(0, 0, Count::NO); };
+    static QuadEqRoots InfRoots() { return QuadEqRoots(0, 0, Count::INF); };
+private:
+    QuadEqRoots(const Complex& x1, const Complex& x2, const Count& count) :
+        _x1(x1), _x2(x2), _count(count) {
+    }
+
+    Complex _x1, _x2;
+    Count _count;
+};
+
+struct QuadEq {
+    double a, b, c;
+    QuadEq() : a(0), b(0), c(0) {}
+    QuadEq(const double a_, const double b_, const double c_) : a(a_), b(b_), c(c_) {}
+};
