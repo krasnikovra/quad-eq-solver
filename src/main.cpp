60dfbec7cc464c0f62485d4ff5181a81afe87356
krasnikovra <roma_krasnikov02@mail.ru>
2022-03-24 15:56:05 +0300

    no noexcepts here

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,13 @@ public:
     QuadEqRoots(const Complex& x) : _x1(x), _x2(x), _count(Count::ONE) {}
     QuadEqRoots(const Complex& x1, const Complex& x2) : _x1(x1), _x2(x2), 
         _count(x1 == x2 ? Count::ONE : Count::TWO) {}
-    Complex GetX1() const noexcept { return _x1; }
-    Complex GetX2() const noexcept { return _x2; }
-    Count GetCount() const noexcept { return _count; }
-    static QuadEqRoots NoRoots() noexcept { return QuadEqRoots(0, 0, Count::NO); };
-    static QuadEqRoots InfRoots() noexcept { return QuadEqRoots(0, 0, Count::INF); };
+    Complex GetX1() const { return _x1; }
+    Complex GetX2() const { return _x2; }
+    Count GetCount() const { return _count; }
+    static QuadEqRoots NoRoots() { return QuadEqRoots(0, 0, Count::NO); };
+    static QuadEqRoots InfRoots() { return QuadEqRoots(0, 0, Count::INF); };
 private:
-    QuadEqRoots(const Complex& x1, const Complex& x2, const Count& count) noexcept :
+    QuadEqRoots(const Complex& x1, const Complex& x2, const Count& count) :
         _x1(x1), _x2(x2), _count(count) {
     }
 
